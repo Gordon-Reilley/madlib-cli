7ce8ae6362587116717a039d0d2fe50e0cd68af4
Gordon P Reilley Jr <gordie378@gmail.com>
2023-01-12 15:07:05 -0500

    finished Lab 03

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,17 +1,4 @@
-def read_template(file):
-    try:
-        with open(file) as f:
-            return f.read()
-    except FileNotFoundError as fnf_error:
-        raise fnf_error
-
-
-# def parse_template(string):
-
-
-def merge(stripped, inputs):
-    return stripped.format(*inputs)
-
+import re
 
 intro = """
 ************************************************
@@ -24,3 +11,34 @@ intro = """
 **                                            **
 ************************************************
 """
+
+def read_template(file):
+    try:
+        with open(file) as d:
+            return d.read()
+    except FileNotFoundError as fnf_error:
+        raise fnf_error
+
+
+def parse_template(string):
+    pieces = tuple(re.findall(r"{([^{}]*)}", string))
+    for x in pieces:
+        string = string.replace(x, "")
+    return string, pieces
+
+
+def merge(stripped, inputs):
+    return stripped.format(*inputs)
+
+
+print(intro)
+script = read_template("examples/video_game_ex.txt")
+empty_string, parts = parse_template(script)
+filled_list = []
+for i in parts:
+    user_input = input(f" Enter {i} > ")
+    filled_list.append(user_input)
+result = merge(empty_string, filled_list)
+print(result)
+with open('output.txt', 'w') as writer:
+    writer.write(result)
